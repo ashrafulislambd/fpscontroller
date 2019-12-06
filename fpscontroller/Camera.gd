7702b95bc01e6615b4e241a918e4ffdc58de89a1
Adam Brown <adamwbrown@gmail.com>
2019-12-06 15:13:23 -0800

    Added ability to "hold" an object
    - Held objects rotate properly with the camera
    - Added ability to lose and regain mouse focus, so exit isn't the only option. You can now move the window around easily.

--- fpscontroller/Camera.gd
+++ fpscontroller/Camera.gd
@@ -1,8 +1,13 @@
 extends Camera
+class_name FpsCamera
+
+onready var fpsController: Spatial = get_parent()
 
 var sensitivity_y = 0
 var inversion_mult = 1
 var max_y = 89
+
+var heldObject: Spatial
 
 func initializeComponents():
 	sensitivity_y = self.get_parent().Sensitivity_Y
@@ -16,9 +21,16 @@ func _ready():
 	self.initializeComponents()
 
 func _input(event):
+	# Don't process input if we aren't capturing the mouse
+	if not fpsController.mouseCaptured:
+		return
+	
 	if event is InputEventMouseMotion:
 		if inversion_mult * sensitivity_y * event.relative.y >= 0 and self.rotation_degrees.x >= max_y:
 			return
 		if inversion_mult * sensitivity_y * event.relative.y <= 0  and self.rotation_degrees.x <= -max_y:
 			return
 		rotate_x(inversion_mult * sensitivity_y * event.relative.y)
+		
+		if heldObject != null:
+			heldObject.rotate_x(inversion_mult * sensitivity_y * event.relative.y)
--- fpscontroller/FpsController.gd
+++ fpscontroller/FpsController.gd
@@ -1,31 +1,61 @@
 extends KinematicBody
 class_name FpsController
 
-export var Sensitivity_X = 0.01
-export var Sensitivity_Y = 0.005
-export var Invert_Y_Axis = false
-export var Exit_On_Escape = true
-export var Maximum_Y_Look = 45
-export var Walk_Accelaration = 3
-export var Maximum_Walk_Speed = 5
-export var Sprint_Accelaration = 6
-export var Maximum_Sprint_Speed = 10
-export var Jump_Speed = 4
-export var Gravity = 9.8
+export(float) var Sensitivity_X := 0.01
+export(float) var Sensitivity_Y := 0.005
+export(bool) var Invert_Y_Axis := false
+export(bool) var Exit_On_Escape := true
+export(float) var Maximum_Y_Look := 45
+export(float) var Walk_Accelaration := 3.0
+export(float) var Maximum_Walk_Speed := 5.0
+export(float) var Sprint_Accelaration := 6.0
+export(float) var Maximum_Sprint_Speed := 10.0
+export(float) var Jump_Speed := 4.0
+export(float) var Gravity := 9.8
 
-var velocity = Vector3(0,0,0)
-var forward_velocity = 0
-var Movement_Speed = 0
+onready var camera := $Camera as FpsCamera
+
+var velocity := Vector3(0,0,0)
+var forward_velocity := 0.0
+var Movement_Speed := 0.0
+
+export(NodePath) var HeldObjectPath: NodePath
+var heldObject: Spatial setget held_object_set, held_object_get
+
+func held_object_set(value: Spatial):
+	heldObject = value
+	self.camera.heldObject = self.heldObject
+	
+func held_object_get() -> Spatial:
+	return heldObject
+
+var mouseCaptured setget mouse_captured_set, mouse_captured_get
+
+func mouse_captured_set(value: bool):
+	mouseCaptured = value
+	
+	if mouseCaptured:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	else:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+func mouse_captured_get() -> Spatial:
+	return mouseCaptured
 
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	self.mouseCaptured = true
 	forward_velocity = Movement_Speed
 	set_process(true)
+	
+	self.heldObject = get_node_or_null(HeldObjectPath)
 
 func _process(delta):
 	if Exit_On_Escape:
 		if Input.is_key_pressed(KEY_ESCAPE):
 			get_tree().quit()
+	else:
+		if Input.is_key_pressed(KEY_ESCAPE):
+			self.mouseCaptured = false
 
 func _physics_process(delta):
 	velocity.x = 0
@@ -77,5 +107,15 @@ func _physics_process(delta):
 	velocity = move_and_slide(velocity, Vector3(0,1,0))
 
 func _input(event):
+	# Don't process input if we aren't capturing the mouse
+	if not self.mouseCaptured:
+		return
+	
 	if event is InputEventMouseMotion:
 		rotate_y(-Sensitivity_X * event.relative.x)
+
+func _notification(what):
+	if what == MainLoop.NOTIFICATION_WM_FOCUS_IN:
+		self.mouseCaptured = true
+	elif what == MainLoop.NOTIFICATION_WM_FOCUS_OUT:
+		self.mouseCaptured = false
